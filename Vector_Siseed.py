9c7ec1540936ec1054fd83c0f89d0f7c8c94c2bf
ketro sithole <u18312889@tuks.co.za>
2025-07-02 18:49:47 +0200

    vectors

---
+++ Vector_Siseed.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# 1D vector
+vec_1d = np.array([1, 2, 3])
+
+# 2D vector (can be thought of as a point or vector in a plane)
+vec_2d = np.array([1, 2])
+
+# 3D vector (point or direction in 3D space)
+vec_3d = np.array([1, 2, 3])
+
+# 4D vector (4D space, e.g., homogeneous coordinates)
+vec_4d = np.array([1, 2, 3, 4])
+
+print("1D vector:", vec_1d)
+print("2D vector:", vec_2d)
+print("3D vector:", vec_3d)
+print("4D vector:", vec_4d)
